253783d105928401b3ca0ca7457cbf952ba1f30f
Abigail <abigail@abigail.freedom.nl>
2022-12-02 21:59:35 +0100

    Add some comments to the solution of day 2.

--- Day_02/solution.pl
+++ Day_02/solution.pl
@@ -13,12 +13,23 @@ use List::Util qw [sum];
 
 @ARGV = "input" unless @ARGV;
 
+#
+# There are only 9 different combinations of both players
+# throwing either rock, paper, or scissors. So, we just
+# precalculate the score of each outcome. We can then 
+# calculate the total by looking up each "hand", and 
+# summing the total.
+#
+
 my ($ELF_ROCK, $ELF_PAPER, $ELF_SCISSORS) = qw [A B C];
 my ( $ME_ROCK,  $ME_PAPER,  $ME_SCISSORS) = qw [X Y Z];
 my ( $TO_LOSE,  $TO_DRAW,        $TO_WIN) = qw [X Y Z];
 my ( $SH_ROCK,  $SH_PAPER,  $SH_SCISSORS) = qw [1 2 3];
 my (     $WIN,      $DRAW,         $LOSS) = qw [6 3 0];
 
+#
+# Hand scores for part 1.
+#
 my %score1 = (
     "$ELF_ROCK $ME_ROCK"          => $DRAW + $SH_ROCK,
     "$ELF_ROCK $ME_PAPER"         => $WIN  + $SH_PAPER,
@@ -31,6 +42,9 @@ my %score1 = (
     "$ELF_SCISSORS $ME_SCISSORS"  => $DRAW + $SH_SCISSORS,
 );
 
+#
+# Hand scores for part 2.
+#
 my %score2 = (
     "$ELF_ROCK $TO_DRAW"          => $DRAW + $SH_ROCK,
     "$ELF_ROCK $TO_WIN"           => $WIN  + $SH_PAPER,
@@ -43,7 +57,13 @@ my %score2 = (
     "$ELF_SCISSORS $TO_DRAW"      => $DRAW + $SH_SCISSORS,
 );
 
+#
+# Processing the input
+#
 chomp (my @input = <>);
 
 say "Solution 1: ", sum map {$score1 {$_}} @input;
 say "Solution 2: ", sum map {$score2 {$_}} @input;
+
+
+__END__
