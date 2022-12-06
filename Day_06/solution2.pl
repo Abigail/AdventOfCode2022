e463b04a46bc2b4554029062815f3a3ee7299346
Abigail <abigail@abigail.freedom.nl>
2022-12-06 14:31:15 +0100

    Second solution for Day 6

---
+++ Day_06/solution2.pl
@@ -0,0 +1,20 @@
+#!/opt/perl/bin/perl
+
+use 5.032;
+
+use strict;
+use warnings;
+no  warnings 'syntax';
+
+use experimental 'signatures';
+use experimental 'lexical_subs';
+
+@ARGV = "input" unless @ARGV;
+
+$_ = <>;
+ /(.{4})(??{$1 =~ m{(.).*\1} ? "(*FAIL)" : ""})/
+                  and say "Solution 1: ", $+ [-1];
+/(.{14})(??{$1 =~ m{(.).*\1} ? "(*FAIL)" : ""})/
+                  and say "Solution 1: ", $+ [-1];
+
+__END__
