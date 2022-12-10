263e02358eb829303676626574dd5cf82012696e
Abigail <abigail@abigail.freedom.nl>
2022-12-10 01:04:30 +0100

    Solution for day 9, part 2

--- Day_09/solution.pl
+++ Day_09/solution.pl
@@ -10,41 +10,22 @@ use experimental 'signatures';
 use experimental 'lexical_subs';
 
 @ARGV = "input" unless @ARGV;
-# @ARGV = "example-1";
 
-my $STRING_LENGTH = 2;
-my @string = map {[0, 0]} 1 .. $STRING_LENGTH;
+my $STRING_LENGTH = 9;
+my @string = map {[0, 0]} 0 .. $STRING_LENGTH;
 
-my ($head_x, $head_y, $tail_x, $tail_y) = (0) x 4;
-my %visited1;
+my %visited_1;
+my %visited_t;
 
 my $X = 0;
 my $Y = 1;
-
-sub show {
-    foreach my $y (reverse 0 .. 4) {
-        foreach my $x (0 .. 5) {
-            my $field = ".";
-            foreach my $i (keys @string) {
-                if ($string [$i] [$X] == $x && $string [$i] [$Y] == $y) {
-                    $field = $i || "H";
-                    last;
-                }
-            }
-            print $field;
-        }
-        print "\n";
-    }
-    print "\n";
-}
 
 while (<>) {
     my ($direction, $distance) = split;
     #
     # Move the head
     #
-    # say "== $direction $distance ==";
-    foreach (1 .. $distance) {
+    foreach my $d (1 .. $distance) {
         #
         # Update the head
         #
@@ -56,23 +37,43 @@ while (<>) {
         #
         # Update the rest of the string
         #
-        foreach my $piece (1 .. $STRING_LENGTH - 1) {
+        foreach my $piece (1 .. $STRING_LENGTH) {
             #
-            # If the piece is touching the previous piece after it moves
-            # the piece will not move
+            # Case 1: If the current piece is still touching the previous piece
+            #         after, it moves the current piece will not move
             #
             if (abs ($string [$piece - 1] [$X] - $string [$piece] [$X]) <= 1 &&
                 abs ($string [$piece - 1] [$Y] - $string [$piece] [$Y]) <= 1) {
                 next;
             }
+
             #
-            # Otherwise, the previous piece is a knights move away from the
-            # current piece.
-            # Move the current piece in the same row/column where the
-            # distance is 1 and next to the current piece in the
-            # row/column where the distance is 2.
+            # Case 2: If, after moving, the previous piece is as far away
+            #         from the current piece in both the row and column,
+            #         move the current one diagonally away from the previous
+            #         piece.
             #
-            if (abs ($string [$piece - 1] [$X] - $string [$piece] [$X]) == 2) {
+            if (abs ($string [$piece - 1] [$X] - $string [$piece] [$X]) ==
+                abs ($string [$piece - 1] [$Y] - $string [$piece] [$Y])) {
+                $string [$piece] [$X] =
+                         $string [$piece] [$X] < $string [$piece - 1] [$X]
+                                               ? $string [$piece - 1] [$X] - 1 
+                                               : $string [$piece - 1] [$X] + 1;
+                $string [$piece] [$Y] =
+                         $string [$piece] [$Y] < $string [$piece - 1] [$Y]
+                                               ? $string [$piece - 1] [$Y] - 1
+                                               : $string [$piece - 1] [$Y] + 1;
+                next;
+            }
+
+            #
+            # Case 3: The previous piece moved away to a place further
+            #         away one axis than the other. In that case move
+            #         the current piece to the axis as the axis for which
+            #         the distance is minimal, and one away in the other.
+            #
+            if (abs ($string [$piece - 1] [$X] - $string [$piece] [$X]) >
+                abs ($string [$piece - 1] [$Y] - $string [$piece] [$Y])) {
                 $string [$piece] [$X] =
                          $string [$piece] [$X] < $string [$piece - 1] [$X]
                                                ? $string [$piece - 1] [$X] - 1 
@@ -87,21 +88,11 @@ while (<>) {
                 $string [$piece] [$X] =          $string [$piece - 1] [$X];
             }
         }
-        # show ();
 
-        $visited1 {$string [1] [$X], $string [1] [$Y]} ++;
+        $visited_1 {$string [ 1] [$X], $string [ 1] [$Y]} ++;
+        $visited_t {$string [-1] [$X], $string [-1] [$Y]} ++;
     }
 }
 
-=pod
-
-foreach my $y (reverse (($min_y - 1) .. ($max_y + 1))) {
-    foreach my $x (($min_x - 1) .. ($max_x + 1)) {
-        print $visited {$x, $y} ? "#" : ".";
-    }
-    print "\n";
-}
-
-=cut
-
-say "Solution 1: ", scalar keys %visited1;
+say "Solution 1: ", scalar keys %visited_1;
+say "Solution 2: ", scalar keys %visited_t;
